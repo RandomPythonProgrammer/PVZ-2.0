169b601853b8b63dcb42d289fa97831fbe4c04de
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2022-02-28 21:53:35 -0600

    yes

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -6,15 +6,18 @@ events: List[pygame.event.Event] = []
 
 
 def set_cancelled(status: bool):
+    """Sets whether the click event has been done"""
     global __cancelled
     __cancelled = status
 
 
 def get_cancelled() -> bool:
+    """Returns whether the click event has been done"""
     return __cancelled
 
 
 def get_events():
+    """Returns all of the events from this game loop"""
     global events
     events.clear()
     events.extend(pygame.event.get())
